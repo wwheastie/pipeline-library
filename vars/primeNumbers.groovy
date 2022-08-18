cf067dc4797b7329f0dd173027b1d09cad15b73b
Whitney Heastie <wwheastie@outlook.com>
2022-08-18 16:31:10 -0500

    Update primeNumbers.groovy

--- vars/primeNumbers.groovy
+++ vars/primeNumbers.groovy
@@ -1,4 +1,7 @@
-def call(Map config = [:], Closure body) {
+@Grab('org.apache.commons:commons-math3:3.4.1')
+import org.apache.commons.math3.primes.Primes
+
+def call() {
   var isPrime = Primes.isPrime(1)
   sh "echo ${isPrime}"
 }
