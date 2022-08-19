92092ed372e7305301f43ff773ec85f635a054c4
Whitney Heastie <wwheastie@outlook.com>
2022-08-19 11:55:25 -0500

    Update generateToken.groovy

--- vars/generateToken.groovy
+++ vars/generateToken.groovy
@@ -1,14 +1,20 @@
 @Grab('com.google.auth:google-auth-library-oauth2-http:1.3.0')
+@Grab('com.fasterxml.jackson.core:jackson-core:2.9.4')
+@Grab('com.fasterxml.jackson.core:jackson-core:2.9.4')
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.json.webtoken.JsonWebSignature;
 import com.google.auth.oauth2.IdToken;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.auth.oauth2.TokenVerifier;
 
 def call() {
+   ObjectMapper mapper = new ObjectMapper();
+   @SuppressWarnings("unchecked")
+   Map<String, Object> json = mapper.readValue(jsonString, HashMap.class);
    String clientId = "test";
    String clientEmail = "test";
    String privateKeyPcks8 = "test";
    String privateKeyId = "test";
-   sh 'echo ev:${CREDENTIALS_JSON}'
+   sh 'echo data:${json}'
    sh 'echo token generated!'
 }
