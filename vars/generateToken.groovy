71ec88291404693b1b3d7a02bbb32bdb16467a57
Whitney Heastie <wwheastie@outlook.com>
2022-08-19 12:15:46 -0500

    Update generateToken.groovy

--- vars/generateToken.groovy
+++ vars/generateToken.groovy
@@ -9,6 +9,7 @@ import com.google.auth.oauth2.TokenVerifier;
 
 def call() {
    def json = new groovy.json.JsonSlurper().parseText(CREDENTIALS_JSON);
+   sh 'echo ${json}'
    String clientId = "test";
    String clientEmail = "test";
    String privateKeyPcks8 = "test";
