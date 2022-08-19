8baab1cf7279672009f147ecfe59bf6753c1ae59
Whitney Heastie <wwheastie@outlook.com>
2022-08-19 12:02:17 -0500

    Update generateToken.groovy

--- vars/generateToken.groovy
+++ vars/generateToken.groovy
@@ -9,6 +9,7 @@ import com.google.auth.oauth2.TokenVerifier;
 
 def call() {
    ObjectMapper mapper = new ObjectMapper();
+   sh 'echo ${CREDENTIALS_JSON.getClass()'}
    @SuppressWarnings("unchecked")
    Map<String, Object> json = mapper.readValue(CREDENTIALS_JSON, HashMap.class);
    String clientId = "test";
